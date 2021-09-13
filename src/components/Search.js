d391ae86ff14f98879fa231149f73704d3f0098f
MiranAmin <61462695+MiranAmin@users.noreply.github.com>
2021-09-13 09:10:53 +0100

    Search bar additional functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,59 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
 
-  console.log("I run with every render");
   useEffect(() => {
-    console.log("I run after every render and initial render");
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search)
+    };
+
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500)
+
+      return () => {
+        clearTimeout(timeoutId)
+      };
+    }
   }, [term]);
 
-  const onTermChange = (e) => {
-    setTerm(e.target.value);
-  };
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">
+            {result.title}
+            <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+          </div>
+        </div>
+        <hr />
+      </div>)
+  })
+
   return (
     <div>
       <div className="ui form">
@@ -18,10 +61,13 @@ const Search = () => {
           <label>Enter Search Term</label>
           <input
             value={term}
-            onChange={onTermChange}
+            onChange={(e) => setTerm(e.target.value)}
             className="input"
           />
         </div>
+      </div>
+      <div className="ui called list">
+        {renderedResults}
       </div>
     </div>
   );
